d42f4477f659fa766aeee2d29bb59ff1f93f971f
Kathan Natrajan <kathan_369@yahoo.com>
2015-02-09 01:38:16 -0700

    simple form gem nigas

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150209041026) do
+ActiveRecord::Schema.define(:version => 20150209074329) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
