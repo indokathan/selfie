5b450145c509066b732287b9238761adb69c1d8e
Kathan Natrajan <kathan_369@yahoo.com>
2015-02-08 19:09:56 -0700

    new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Selfie::Application.config.secret_token = '4230644ccf1420f04d74495b6d0a7d16b1286a010400c42e3401536f90e1863ea260c65cfe9ad125235c8193b5ad693155ce3f1223ae9159f52c0a8e095d19b6'
