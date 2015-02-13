a1c3b3dd45dcc1d261ecb681a42751725a538f2c
Kathan Natrajan <kathan_369@yahoo.com>
2015-02-12 21:49:25 -0700

    add username model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Selfie::Application.routes.draw do
 
+  resources :pins
+
+
   devise_for :users
 
   get 'about' => 'Pages#about'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150212063450) do
+ActiveRecord::Schema.define(:version => 20150212215146) do
+
+  create_table "pins", :force => true do |t|
+    t.string   "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
 # Could not dump table "users" because of following StandardError
 #   Unknown type 'sting' for column 'username'
